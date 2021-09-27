88dfc8520a2529107774f518e5b707c47535bed4
hxckey <george.h@live.com>
2021-09-27 17:35:27 +0100

    update html add unsplash api fetch

--- motivationalAPI.js
+++ motivationalAPI.js
@@ -1,5 +1,26 @@
+
+import { createApi } from "unsplash-js"
+
 const generator = document.getElementById('generator')
+const randomURL = 'https://api.unsplash.com/photos'
 
 generator.addEventListener('click', () => {
     console.log('You found the button!')
+    randomImage()
 })
+
+const unsplash = createApi({
+    accessKey: "3OOIocMsyGJHlyPQuL8HyPBGYgglRp6g976uDjVqzdw"
+});
+
+function randomImage() {
+    unsplash.photos.getRandom
+    .then(result => {
+        if(result.errors) {
+            console.log('error: ', result.errors[0]);
+        } else {
+            const photo = result.response;
+        }
+    })
+};
+
