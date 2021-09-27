754c3f08e4f1d70c4144dea02661c58367dc3ddf
hxckey <george.h@live.com>
2021-09-27 18:04:22 +0100

    restructure fetch request

--- motivationalAPI.js
+++ motivationalAPI.js
@@ -4,23 +4,35 @@ import { createApi } from '/unsplash-js'
 const generator = document.getElementById('generator')
 const randomURL = 'https://api.unsplash.com/photos'
 
-generator.addEventListener('click', () => {
-    console.log('You found the button!')
-    randomImage()
-})
 
 const unsplash = createApi({
     accessKey: "3OOIocMsyGJHlyPQuL8HyPBGYgglRp6g976uDjVqzdw"
 });
 
-function randomImage() {
-    unsplash.photos.getRandom
-    .then(result => {
-        if(result.errors) {
-            console.log('error: ', result.errors[0]);
-        } else {
-            const photo = result.response;
-        }
-    })
-};
+// function randomImage() {
+//     unsplash.photos.getRandom
+//     .then(result => {
+//         if(result.errors) {
+//             console.log('error: ', result.errors[0]);
+//         } else {
+//             const photo = result.response;
+//         }
+//     })
+// };
 
+function randomImage(){
+    try{
+        const url = 'https://api.unsplash.com/photos/random'
+        fetch(url)
+        .then(response =>response.json())
+        .then(data => );
+    } catch (err) {
+        console.log(err);
+    }
+    };
+}
+
+generator.addEventListener('click', () => {
+    console.log('You found the button!')
+    randomImage()
+})
