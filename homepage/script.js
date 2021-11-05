fa2af42212d5bf284078362afc924714e28aa0eb
hxckey <george.h@live.com>
2021-11-05 11:10:25 +0000

    update script.js

--- homepage/script.js
+++ homepage/script.js
@@ -1,12 +1,14 @@
 const wave = document.getElementById('wave')
 const name = document.getElementById('george')
 
-wave.addEventListener('mouseenter', () => {
+wave.addEventListener('mouseover', () => {
     wave.style.height = '80%'
     george.style.paddingTop = '0'
+    george.style.fontSize = '5rem'
 })
 
 wave.addEventListener('mouseleave', () => {
     wave.style.height = ''
     george.style.paddingTop = '10rem'
+    george.style.fontSize = '7rem'
 })
