98ceca5b540d1a76bf4f10bbb7347f5ee9c2a8a2
hxckey <george.h@live.com>
2021-11-05 15:38:01 +0000

    add links to landing page, finalise landing page styling

--- homepage/script.js
+++ homepage/script.js
@@ -1,6 +1,8 @@
 const wave = document.getElementById('wave')
 const name = document.getElementById('george')
-const linkSpace = document.getElementById('links')
+const linkSpace = document.getElementById('wave-section')
+let about = document.getElementById('about')
+let projects = document.getElementById('projects');
 
 
 wave.addEventListener('mouseover', () => {
@@ -11,6 +13,7 @@ wave.addEventListener('mouseover', () => {
     george.style.fontSize = '5rem'
     wave.style.left = '-5'
     wave.style.transform = 'scaleY(2)'
+    showLinks();
 })
 
 wave.addEventListener('mouseleave', () => {
@@ -19,4 +22,17 @@ wave.addEventListener('mouseleave', () => {
     george.style.paddingBottom = '0'
     george.style.fontSize = '7rem'
     wave.style.transform = 'scaleY(1)'
+    hideLinks();
 })
+
+const showLinks = () => {
+    projects.style.display = 'inline-block'
+    about.style.display = 'inline-block'
+    projects.style.transform = 'scaleX(1.6)';
+    about.style.transform = 'scaleX(1.6)';
+}
+
+const hideLinks = () => {
+    projects.style.display = 'none'
+    about.style.display = 'none'
+}
